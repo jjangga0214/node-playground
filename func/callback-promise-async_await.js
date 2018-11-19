3b8c74c662a71fe679e8d2a75795b6b0e2672600
Unknown <jjangga@kookmin.ac.kr>
2018-11-19 11:37:00 +0900

    add: async, await

--- func/callback-promise-async_await.js
+++ func/callback-promise-async_await.js
@@ -7,7 +7,7 @@ function someAsync(callback) {
 }
 
 function someAsync2(data, callback) {
-  const [err, data2] = ['err is occurred', null];
+  const [err, data2] = ['err is occurred in someAsync2', null];
   setTimeout(() => callback(err, data2), 0);
 }
 
@@ -25,17 +25,25 @@ someAsync(function (err, data) {
   }
 });
 
+/**
+ * @param data
+ * @returns {Promise<any>}
+ */
 function somePromising(data) {
 
   return new Promise((resolve, reject) => {
     if (data === 'data') {
-      resolve('resolved data');
+      resolve('resolved data on somePromising');
     } else {
-      reject('rejected');
+      reject('rejected on somePromising');
     }
   });
 }
 
+/**
+ * @param data
+ * @returns {Promise<never>}
+ */
 function somePromising2(data) {
   return Promise.reject('rejected on somePromising2');
 }
@@ -52,3 +60,40 @@ promised
   .catch(err => {
     console.log(err);
   });
+
+Promise.all([promised, Promise.resolve('data2'), Promise.reject('rejected3'), Promise.resolve('data3'),])
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+/**
+ * async function, which takes awaited Promise calculated value.
+ * @returns {Promise<void>}
+ */
+async function run() {
+  try {
+    const data1 = await somePromising('data');
+    const data2 = await somePromising2(data1);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run();
+
+/**
+ * in async function
+ * for await snippet pattern,
+ * which replaces `Promise.all(Array<Promise>)`
+ */
+const promises = [Promise.resolve('data1'), Promise.reject('rejected2'), Promise.resolve('not accessed')];
+
+(async () => {
+  try {
+    for await (const promise of promises) {
+      console.log(promise);
+    }
+    // err 가 발생하면 루프는 완전히 종료되고 catch 로 이동한다.
+  } catch (error) {
+    console.log(error);
+  }
+})();
